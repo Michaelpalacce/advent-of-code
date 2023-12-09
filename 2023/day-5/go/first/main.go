cc147409e7ef607243057c9ed5a898fe184c599e
Stefan Genov <sgenov@sgenov7HCQN.vmware.com>
2023-12-10 00:19:19 +0200

    feat: progress

--- 2023/day-5/go/first/main.go
+++ 2023/day-5/go/first/main.go
@@ -1,10 +1,9 @@
-package main
+package first
 
 import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -17,7 +16,7 @@ type Mapping struct {
 }
 
 func main() {
-	filePath := "debug.txt"
+	filePath := "input.txt"
 
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -28,13 +27,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	scanner.Scan()
-	rawSeeds := parseNumbers(scanner.Text())
-
-	// Split seeds into touples
-	seedRanges := make([][]int, 0)
-	for i := 0; i < len(rawSeeds); i += 2 {
-		seedRanges = append(seedRanges, []int{rawSeeds[i], rawSeeds[i+1]})
-	}
+	seeds := parseNumbers(scanner.Text())
 
 	// Arbitrarily read the next line
 	scanner.Scan()
@@ -48,57 +41,50 @@ func main() {
 	temperatureToHumidityMap := readMap(scanner)
 	humidityToLocationMap := readMap(scanner)
 	lowestLocation := -1
-	for _, seedRange := range seedRanges {
-		fmt.Println("seed", seedRange)
-		soil := findOverlapAndReturnDestination(seedRange, seedToSoilMap)
+	for _, seed := range seeds {
+		fmt.Println("seed", seed)
+		soil := findMappingAndReturnDestination(seed, seedToSoilMap)
 		fmt.Println("soil", soil)
-		fertilizer := findOverlapAndReturnDestination(soil, soilToFertilizerMap)
+		fertilizer := findMappingAndReturnDestination(soil, soilToFertilizerMap)
 		fmt.Println("fertilizer", fertilizer)
-		water := findOverlapAndReturnDestination(fertilizer, fertilizerToWaterMap)
+		water := findMappingAndReturnDestination(fertilizer, fertilizerToWaterMap)
 		fmt.Println("water", water)
-		light := findOverlapAndReturnDestination(water, waterToLightMap)
+		light := findMappingAndReturnDestination(water, waterToLightMap)
 		fmt.Println("light", light)
-		temperature := findOverlapAndReturnDestination(light, lightToTemperatureMap)
+		temperature := findMappingAndReturnDestination(light, lightToTemperatureMap)
 		fmt.Println("temperature", temperature)
-		humidity := findOverlapAndReturnDestination(temperature, temperatureToHumidityMap)
+		humidity := findMappingAndReturnDestination(temperature, temperatureToHumidityMap)
 		fmt.Println("humidity", humidity)
-		location := findOverlapAndReturnDestination(humidity, humidityToLocationMap)
+		location := findMappingAndReturnDestination(humidity, humidityToLocationMap)
 		fmt.Println("location", location)
 
 		if lowestLocation == -1 {
-			lowestLocation = location[0]
+			lowestLocation = location
 		}
 
-		if location[0] < lowestLocation {
-			lowestLocation = location[0]
+		if location < lowestLocation {
+			lowestLocation = location
 		}
 	}
 
 	fmt.Println("lowestLocation", lowestLocation)
 }
 
-// The only thing missing for the solutino is that I need to still return the unmatched ones :/
-func findOverlapAndReturnDestination(input []int, mappings []Mapping) []int {
-	for _, mapping := range mappings {
-		a1, a2 := float64(mapping.Source), float64(mapping.Source+mapping.Range)
-		b1, b2 := float64(input[0]), float64(input[0]+input[1])
-
-		// Check for overlap
-		overlapStart := math.Max(a1, b1)
-		overlapEnd := math.Min(a2, b2)
-
-		if overlapStart < overlapEnd {
-			fmt.Println("overlap", overlapStart, overlapEnd)
-			overlapLength := overlapEnd - overlapStart
-			return []int{int(mapping.Destination +(int(overlapStart) - mapping.Source)), int(overlapLength)}
-		}
-	}
-
-	return input
-}
-
 func findMappingAndReturnDestination(input int, mappings []Mapping) int {
 	for _, mapping := range mappings {
+		// fmt.Println("")
+		// fmt.Println("")
+		// fmt.Println("")
+		// fmt.Println("mapping", mapping)
+		// fmt.Println("input", input)
+		// fmt.Println("mapping.Source", mapping.Source)
+		// fmt.Println("mapping.Range", mapping.Range)
+		// fmt.Println("mapping.Destination", mapping.Destination)
+		// fmt.Println("mapping.Source < input", mapping.Source < input)
+		// fmt.Println("mapping.Source+mapping.Range > input", mapping.Source+mapping.Range > input)
+		// fmt.Println("")
+		// fmt.Println("")
+		// fmt.Println("")
 		if mapping.Source < input && mapping.Source+mapping.Range > input {
 			return mapping.Destination + (input - mapping.Source)
 		}
