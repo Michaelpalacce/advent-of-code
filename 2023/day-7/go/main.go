70c43924667f14222409a9b109cd608ee4aa5cff
Stefan Genov <stefantigro@gmail.com>
2023-12-14 22:40:56 +0200

    feat: input + debug for day-7

---
+++ 2023/day-7/go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	filePath := "input.txt"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+}
