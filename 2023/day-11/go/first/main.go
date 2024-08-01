c26227cbf8f3a5a4dd845d81fea9f8c7909bc074
Stefan Genov <stefan.genov@broadcom.com>
2024-08-01 15:15:05 +0300

    feat: day 11 first and second in one solution

--- 2023/day-11/go/first/main.go
+++ 2023/day-11/go/first/main.go
@@ -7,14 +7,15 @@ import (
 	"os"
 )
 
-type Pipe struct {
-	Symbol      string
-	Connections []*Pipe
+type Galaxy struct {
+	x int
+	y int
 }
 
 func main() {
-	// filePath := "input.txt"
-	filePath := "debug.txt"
+	cosmicExpansion := 999999
+	filePath := "input.txt"
+	// filePath := "debug.txt"
 
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -24,9 +25,108 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	galaxies := make([]Galaxy, 0)
+	width := 0
+	height := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+
+		if width == 0 {
+			width = len(line)
+		}
+
+		galaxies = append(galaxies, processLine(line, height)...)
+		height++
 	}
+
+	emptyX := make([]int, 0)
+	emptyY := make([]int, 0)
+
+	for x := range width {
+		hasGalaxy := false
+		for _, galaxy := range galaxies {
+			if galaxy.x == x {
+				hasGalaxy = true
+			}
+		}
+
+		if !hasGalaxy {
+			fmt.Printf("No galaxy at x: %d\n", x)
+			emptyX = append(emptyX, x)
+		}
+	}
+
+	for y := range height {
+		hasGalaxy := false
+		for _, galaxy := range galaxies {
+			if galaxy.y == y {
+				hasGalaxy = true
+			}
+		}
+
+		if !hasGalaxy {
+			fmt.Printf("No galaxy at y: %d\n", y)
+			emptyY = append(emptyY, y)
+		}
+	}
+
+	for i := range len(galaxies) {
+		galaxy := &galaxies[i]
+
+		xIncrease := 0
+		for _, x := range emptyX {
+			if galaxy.x > x {
+				xIncrease += cosmicExpansion
+			}
+		}
+
+		yIncrease := 0
+		for _, y := range emptyY {
+			if galaxy.y > y {
+				yIncrease += cosmicExpansion
+			}
+		}
+
+		galaxy.x += xIncrease
+		galaxy.y += yIncrease
+	}
+
+	shortestPathAccumulator := 0
+
+	for len(galaxies) > 0 {
+		galaxy := galaxies[0]
+		galaxies = galaxies[1:]
+
+		for _, otherGalaxy := range galaxies {
+			shortestPathAccumulator += shortestPath(galaxy, otherGalaxy)
+		}
+	}
+
+	fmt.Printf("Shortest path: %d\n", shortestPathAccumulator)
+}
+
+// processLine will find the galaxies in the given line and return them as a slice
+func processLine(line string, y int) []Galaxy {
+	galaxies := make([]Galaxy, 0)
+	for index, char := range line {
+		if string(char) == "#" {
+			galaxies = append(galaxies, Galaxy{
+				x: index,
+				y: y,
+			})
+		}
+	}
+	return galaxies
+}
+
+func shortestPath(galaxy1 Galaxy, galaxy2 Galaxy) int {
+	return Abs((galaxy1.x - galaxy2.x)) + Abs((galaxy1.y - galaxy2.y))
+}
+
+func Abs[T int | int8 | int16 | int32 | int64](x T) T {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
