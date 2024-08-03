f7135494ee4fd339b13c54a48111675dba2db8ba
Stefan Genov <sgenov94@gmail.com>
2024-08-03 13:28:17 +0300

    feat: parsing the grid

--- 2023/day-12/go/first/main.go
+++ 2023/day-12/go/first/main.go
@@ -6,11 +6,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-type Galaxy struct {
-	x int
-	y int
+type Grid struct {
+	Rows []Row
+}
+
+type Row struct {
+	Positions   []Position
+	Contingency []int
+}
+
+const (
+	Onsen   = "."
+	Broken  = "#"
+	Unknown = "?"
+)
+
+type Position struct {
+	X     int
+	Y     int
+	State string
 }
 
 func main() {
@@ -26,10 +44,60 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	height := 0
+	grid := Grid{
+		Rows: make([]Row, 0),
+	}
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(line)
+		grid.Rows = append(grid.Rows, processLine(line, height))
+		height++
+	}
+
+	for _, row := range grid.Rows {
+		fmt.Printf("Now working on row: %v\n", row)
+
+		break
+	}
+}
+
+func processLine(line string, height int) Row {
+	positions := make([]Position, 0)
+
+	lineParts := strings.Split(line, " ")
+
+	if len(lineParts) != 2 {
+		log.Fatalf("Invalid line: %s\n", line)
+	}
+
+	grid := lineParts[0]
+
+	for index, char := range grid {
+		sChar := string(char)
+
+		positions = append(positions, Position{
+			X:     index,
+			Y:     height,
+			State: sChar,
+		})
+	}
+
+	contingencyParts := strings.Split(lineParts[1], ",")
+	contingency := make([]int, 0)
+
+	for _, part := range contingencyParts {
+		partInt, err := strconv.Atoi(part)
+		if err != nil {
+			log.Fatalf("Invalid contingency part: %s\n", part)
+		}
+
+		contingency = append(contingency, partInt)
+	}
+
+	return Row{
+		Positions:   positions,
+		Contingency: contingency,
 	}
 }
